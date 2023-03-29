9e90e0c8b89d9bc857578428dd9bdf94b82a73e1
orangecalvin <kncljmmmm@gmail.com>
2023-03-29 22:56:39 +0900

    김규래 - 타임리프
    main jsp 에서 타임리프로 변경하고 nav는 모듈화시킴

--- src/main/java/ezen/hang/web/controller/WebController.java
+++ src/main/java/ezen/hang/web/controller/WebController.java
@@ -1,10 +1,11 @@
 package ezen.hang.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/thymeleaf")
 public class WebController {
 	@RequestMapping("/main")
 	public String main() {
